6ac07f3adcc56cdea93cc20a898fb379b78a4496
Yenzo-Devos <yenzo.devos@student.howest.be>
2025-02-16 22:04:17 +0100

    Added a basic TextureComponent

---
+++ Minigin/TextureComponent.cpp
@@ -0,0 +1,19 @@
+#include "TextureComponent.h"
+
+dae::TextureComponent::TextureComponent(int x, int y)
+	: BaseComponent()
+	, m_X{ x }, m_Y{ y }
+	, m_pTexture { nullptr }
+{
+}
+
+void dae::TextureComponent::Render()
+{
+	if (!m_pTexture)
+		dae::Renderer::GetInstance().RenderTexture(*m_pTexture, m_X, m_Y);
+}
+
+void dae::TextureComponent::LoadTexture(const std::string& textureName)
+{
+	m_pTexture = ResourceManager::GetInstance().LoadTexture(textureName);
+}
---
+++ Minigin/TextureComponent.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "BaseComponent.h"
+#include "Texture2D.h"
+#include "Renderer.h"
+#include "ResourceManager.h"
+
+#include <memory>
+
+namespace dae
+{
+	class TextureComponent : public BaseComponent
+	{
+	public:
+		TextureComponent(int x, int y);
+		~TextureComponent() = default;
+
+		TextureComponent(const TextureComponent& other) = delete;
+		TextureComponent(TextureComponent&& other) = delete;
+		TextureComponent& operator=(const TextureComponent& other) = delete;
+		TextureComponent& operator=(TextureComponent&& other) = delete;
+
+		void Render() override;
+		void LoadTexture(const std::string& textureName);
+	
+	private:
+		int m_X, m_Y;
+		std::shared_ptr<Texture2D> m_pTexture;
+	};
+}
