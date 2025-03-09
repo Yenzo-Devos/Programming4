5f0d5a851590e1963114d3dc00df6bf144e61f25
Yenzo-Devos <yenzo.devos@student.howest.be>
2025-03-09 22:46:25 +0100

    added functionality to the Command class, adding a MoveCommand and a GameObjectCommand

--- Minigin/Command.cpp
+++ Minigin/Command.cpp
@@ -1,1 +1,19 @@
 #include "Command.h"
+
+dae::GameObjectCommand::GameObjectCommand(GameObject* pGameObject)
+	: Command()
+	, m_pGameObject{ pGameObject }
+{
+}
+
+dae::MoveCommand::MoveCommand(GameObject* pGameObject, float speed, glm::vec3 direction)
+	: GameObjectCommand(pGameObject)
+	, m_Speed{ speed }
+	, m_Direction{ direction }
+{
+}
+
+void dae::MoveCommand::Execute(float deltaTime)
+{
+	GetGameObject()->SetLocalPosition(GetGameObject()->GetWorldPosition() + (m_Direction * m_Speed * deltaTime));
+}
--- Minigin/Command.h
+++ Minigin/Command.h
@@ -1,13 +1,35 @@
 #pragma once
+#include "GameObject.h"
+#include "glm.hpp"
 namespace dae
 {
 	class Command
 	{
 	public:
 		virtual ~Command() = default;
-		virtual void Execute() = 0;
+		virtual void Execute(float) = 0;
+	protected:
+		Command() = default;
 	};
 
 	// create derived command for get an owned object by a controller
+	class GameObjectCommand : public Command
+	{
+	protected:
+		GameObjectCommand(GameObject* pGameObject);
+		GameObject* GetGameObject() const { return m_pGameObject; }
+	private:
+		GameObject* m_pGameObject;
+	};
+
+	class MoveCommand : public GameObjectCommand
+	{
+	public:
+		MoveCommand(GameObject* pGameObject, float speed, glm::vec3 direction);
+		void Execute(float deltaTime) override;
+	private:
+		float m_Speed;
+		glm::vec3 m_Direction;
+	};
 }
 
